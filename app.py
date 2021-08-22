a5a5d8c467c1e9d25bcc5146395ea6526577c34a
808dachun <808dachun@gmail.com>
2021-08-22 02:43:46 -0400

    heroku psql url

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///jiujitsu"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',"postgresql:///jiujitsu")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'hellosecret1')
 
